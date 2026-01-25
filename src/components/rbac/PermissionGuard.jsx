b22ecf7eefb0c800d32db7d43724a3b9339b841c
Fernando Garza <fernandogarzaaa@gmail.com>
2026-01-25 12:40:38 +0000

    File changes

--- src/components/rbac/PermissionGuard.jsx
+++ src/components/rbac/PermissionGuard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { checkPermission } from '@/functions/permissionCheck';
+import { checkPermission } from '../../../functions/permissionCheck';
 
 export default function PermissionGuard({ 
   permission, 
